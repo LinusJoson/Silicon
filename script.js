3f5288e41865f6c235a9b5684946ebbf7f8a2094
LinusJoson <linusjoson@gmail.com>
2024-11-10 22:24:54 +0100

    Addering av Script.js för accordion-meny

---
+++ script.js
@@ -0,0 +1,23 @@
+const questions = document.querySelectorAll('.question');
+
+
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        const parent = question.parentElement;
+
+        if(parent.classList.contains('open')) {
+            parent.classList.remove('open');
+            
+            return
+        }
+        
+        document.querySelectorAll('.faq-card').forEach(answer => {
+            answer.classList.remove('open');
+
+        })
+
+        parent.classList.toggle('open');
+    })
+
+ }) 
